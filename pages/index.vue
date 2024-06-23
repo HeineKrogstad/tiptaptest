21a15ff67c97aca28af729045c8f3937366921c3
Heine <heinekrogstad@gmail.com>
2024-06-23 23:33:49 +0300

    modified:   pages/index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,5 +4,6 @@
 
 
 <style>
-    .draft { @apply flex justify-center }
+    .draft { @apply flex justify-center text-3xl mt-20 gap-4}
+    .draft a { @apply text-cyan-900 bg-gray-100 rounded-lg px-2}
 </style>
